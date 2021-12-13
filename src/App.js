4fb4b0f12636fa2a19620406900064169e266cf8
Heron Amaral <heron.amaral@gmail.com>
2021-12-13 16:24:26 -0300

    Adding wind direction and speed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
+/* API Key and Url */
 const api = {
   key: "5464b2df467576a3f4839ab27d5a3a45",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
+
+// MAIN COMPONENTS
 function App() {
+  // STATES
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState('');
+
+
+  // FUNCTIONS
+  // ASYNC FUNCTIONS CONSUMING OPENWEATHERMAP
   const search = evt => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}
@@ -20,6 +28,7 @@ function App() {
     }
   }
 
+  // BUILD THE DATE THAT WILL BE DISPLAYED IN THE SCREEN
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", 
     "April", "May", "June", "July", "August", 
@@ -35,6 +44,8 @@ function App() {
 
     return `${day} ${date} ${month} ${year}`
   }
+
+  // COMPONENT RETURN
   return (
     <div className={
       (typeof weather.main != "undefined") 
@@ -52,7 +63,9 @@ function App() {
           value={query}
           onKeyPress={search}
           />
+          
         </div>
+        {/* CONDITIONAL RETURN */}
         {(typeof weather.main != "undefined") ? (
           <div>
             <div className="location-box">
@@ -60,11 +73,28 @@ function App() {
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
-              <div className="temp">
-              {Math.round(weather.main.temp)}°C
+              <div className="weather-box-sup">
+                {/* TEMPERATURE */}
+                <div className="temp">
+                {Math.round(weather.main.temp)}°C
+                </div>
               </div>
-              <div className="weather"> 
-              {weather.weather[0].main}
+              <div className="weather-box-bot">
+                {/* WEATHER */}
+                <div className="weather">
+                <img 
+                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} 
+                alt={weather.weather[0].main} /> 
+                {weather.weather[0].main}
+                </div>
+                {/* WIND */}
+                <div className="wind">
+                      <div className="windArea">
+                        <div className="windDirection" style={{transform:`rotate(${weather.wind.deg -90}deg)`}}></div>
+                      </div>
+                      <div className="windTitle">Wind</div>
+                      <div className="windSpeed">{weather.wind.speed}<span>km/h</span></div>
+              </div>        
               </div>
             </div>
           </div>
